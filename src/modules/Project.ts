6bc2ba13c31f53092b0c9168d2eca7a3f96bb781
isabella-projects <damyan4oy@gmail.com>
2024-04-04 12:46:04 +0200

    Separate class for rendering project items & getter for assignees

--- src/modules/Project.ts
+++ src/modules/Project.ts
@@ -94,6 +94,34 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     }
 }
 
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person';
+        } else {
+            return `${this.project.people} persons`;
+        }
+    }
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() {}
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned to this project.';
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
+
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     assignedProjects: Project[];
 
@@ -135,18 +163,17 @@ export class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         listEl.innerHTML = '';
 
         this.assignedProjects.forEach((projectItem, index) => {
-            const listItem = document.createElement('li');
-            listItem.textContent = projectItem.title;
+            const listItem = new ProjectItem(this.element.id, projectItem);
 
             if (this.type === 'active' && index !== lastChildIndex) {
-                listItem.classList.add('visible');
+                listItem.element.classList.add('visible');
             }
 
-            listEl.appendChild(listItem);
+            listEl.appendChild(listItem.element);
 
             if (this.type === 'active' && index === lastChildIndex) {
                 setTimeout(() => {
-                    listItem.classList.add('visible');
+                    listItem.element.classList.add('visible');
                 }, 100);
             }
         });
